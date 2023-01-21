5af083ca77867602dc9485a9edd70ad449dfb9a1
Fran <francisco.leong@gmail.com>
2023-01-21 16:49:18 +0000

    Modi funcion

--- src/translate.py
+++ src/translate.py
@@ -1,14 +1,18 @@
 import json
 import todoList
 
+
 def translate(event, context):
-    item = todoList.get_item(event['pathParameters']['id'])
-    translation = todoList.translate_item(
-        item['text'], event['pathParameters']['language'])
-    return {
-            "statusCode": 200,
-            "body": json.dumps(translation)
-        } if translation else {
+    try:
+        item = todoList.get_item(event['pathParameters']['id'])
+        translation = todoList.translate_item(
+            item['text'], event['pathParameters']['language'])
+        return {
+                "statusCode": 200,
+                "body": json.dumps(translation)
+            }
+    except:
+        return {
             "statusCode": 404,
             "body": ""
         }
