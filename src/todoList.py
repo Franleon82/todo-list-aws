ba1edab7f349fe9c33c710cd64ecbf96e6ca8f60
Fran <francisco.leong@gmail.com>
2023-01-25 21:38:10 +0000

    Se modifica funcion 2

--- src/todoList.py
+++ src/todoList.py
@@ -10,17 +10,16 @@ from botocore.exceptions import ClientError
 def get_table(dynamodb=None):
     table = None
     try:
-         if not dynamodb:
-             URL = os.environ['ENDPOINT_OVERRIDE']
-             if URL:
-                  print('URL dynamoDB:'+URL)
-                  boto3.client = functools.partial(boto3.client, endpoint_url=URL)
-                  boto3.resource = functools.partial(boto3.resource,
-                                               endpoint_url=URL)
-                  dynamodb = boto3.resource("dynamodb")
-    # fetch todo from the database
-                  table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    
+        if not dynamodb:
+            URL = os.environ["ENDPOINT_OVERRIDE"]
+            if URL:
+                print("URL dynamoDB:" + URL)
+                boto3.client = functools.partial(boto3.client, endpoint_url=URL)
+                boto3.resource = functools.partial(boto3.resource, endpoint_url=URL)
+                dynamodb = boto3.resource("dynamodb")
+                # fetch todo from the database
+                table = dynamodb.Table(os.environ["DYNAMODB_TABLE"])
+
     except KeyError as e:
         print(e)
     return table
