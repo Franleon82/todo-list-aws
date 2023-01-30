1f0c317078ac32aef70edfeaa4f43273e09122cd
Fran <francisco.leong@gmail.com>
2023-01-30 23:00:22 +0000

    Arreglados test 8

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -208,16 +208,18 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertRaises(TypeError, delete_item("", self.dynamoFail))
         print ('End: test_delete_todo_error')
 
-    def test_translate_todo(self):
+   def test_translate_todo(self):
         print ('---------------------')
         print ('Start: test_translate_todo')
+        self.table = create_todo_table_language(self.dynamodb)
         from src.todoList import translate_item
-        translation = translate_item(self.text, "fr", self.dynamodb)
-        print ('Response translate fr:' + str(translation))
-        self.assertEqual("Apprenez DevOps et Cloud \u00e1 l'UNIR", translation)
         translation = translate_item(self.text, "en", self.dynamodb)
         print ('Response translate en:' + str(translation))
         self.assertEqual("Learn DevOps and Cloud at UNIR", translation)
+        translation = translate_item(self.text, "fr", self.dynamodb)
+        print ('Response translate fr:' + str(translation))
+        self.assertEqual("Apprenez DevOps et Cloud \u00e1 l'UNIR", translation)
+        "Apprenez DevOps et Cloud à l'UNIR"
         print ('End: test_delete_todo')
 
 
