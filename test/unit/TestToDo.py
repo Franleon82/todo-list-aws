e90872bf19e0f7a1bba99bde19769f5f107b5c4f
Fran <francisco.leong@gmail.com>
2023-01-30 23:57:37 +0000

    Arreglados test

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -209,12 +209,10 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertRaises(TypeError, delete_item("", self.dynamoFail))
         print ('End: test_delete_todo_error')
         
-    def test_translate_todo(self):
+    def test_translate_todo(self):      # pragma: no cover
         print ('---------------------')
         print ('Start: test_translate_todo')
-        #self.table = create_todo_table_language(self.dynamodb)
         from src.todoList import translate_item
-        # Testing file functions
         translation = translate_item(self.text, "en", self.dynamodb)
         print ('Response translate en:' + str(translation))
         self.assertEqual("Learn DevOps and Cloud at UNIR", translation)
